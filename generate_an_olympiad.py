58b26cacfdb58e3c59dbce234a3e370bb73f37aa
Foki4er <128300208+Foki4er@users.noreply.github.com>
2023-04-17 23:02:26 +0400

    Add files via upload

---
+++ generate_an_olympiad.py
@@ -0,0 +1,21 @@
+from flask import redirect, render_template, request, url_for, flash
+from user_data import Olympiad, db
+from werkzeug.security import generate_password_hash
+from flask_login import login_required
+
+class Generate_something:
+    def generate_olympiad(self):
+        if request.method == 'POST':
+            title = request.form['title']
+            introduction = request.form['introduction']
+            text = request.form['text']
+
+            olympiad = Olympiad(title=title, introduction=introduction, text=text)
+            try:
+                db.session.add(olympiad)
+                db.session.commit()
+                return redirect('/сompetitions')
+            except:
+                return 'При добавлении статьи произошла ошибка'
+
+        return render_template('create-article.html')
